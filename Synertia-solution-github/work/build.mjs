35c7dd5ce68141049f12c73537a66654451edc39
mhmdajami <39279656+mhmdajami@users.noreply.github.com>
2026-06-12 21:46:08 +0200

    Delete Synertia-solution-github directory

--- Synertia-solution-github/work/build.mjs
+++
@@ -1,9 +0,0 @@
-import { build } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-await build({
-  root: new URL("..", import.meta.url).pathname.replace(/^\/([A-Z]:)/, "$1"),
-  base: "./",
-  plugins: [react(), tailwindcss()],
-});
